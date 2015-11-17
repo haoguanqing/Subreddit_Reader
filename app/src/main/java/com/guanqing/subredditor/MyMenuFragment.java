5d11f94156dcd9dd880a4f931ffb2f1c496df8e7
Guanqing <haoguanqing@live.com>
2015-11-17 18:48:29 -0500

    add dependencies

---
+++ app/src/main/java/com/guanqing/subredditor/MyMenuFragment.java
@@ -0,0 +1,46 @@
+package com.guanqing.subredditor;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.mxn.soul.flowingdrawer_core.MenuFragment;
+import com.squareup.picasso.Picasso;
+
+
+
+public class MyMenuFragment extends MenuFragment {
+
+    private ImageView ivMenuUserProfilePhoto;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_menu, container,
+                false);
+        ivMenuUserProfilePhoto = (ImageView) view.findViewById(R.id.ivMenuUserProfilePhoto);
+        setupHeader();
+        return  setupReveal(view) ;
+    }
+
+    private void setupHeader() {
+        int avatarSize = getResources().getDimensionPixelSize(R.dimen.global_menu_avatar_size);
+        String profilePhoto = "profile photo";
+        Picasso.with(getActivity())
+                .load(profilePhoto)
+                .placeholder(R.drawable.img_circle_placeholder)
+                .resize(avatarSize, avatarSize)
+                .centerCrop()
+                .transform(new CircleTransformation())
+                .into(ivMenuUserProfilePhoto);
+    }
+
+}
