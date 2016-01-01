8a1e5bea19b43111423726d3c36dcc5b74246ede
Guanqing <haoguanqing@live.com>
2016-01-01 08:40:42 -0800

    clean code and add comments

--- app/src/main/java/com/guanqing/subredditor/FrontPageAdapter.java
+++ app/src/main/java/com/guanqing/subredditor/FrontPageAdapter.java
@@ -1,11 +1,7 @@
 package com.guanqing.subredditor;
 
 import android.content.Context;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,42 +10,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.guanqing.subredditor.Retrofit.ImgurClient;
-import com.guanqing.subredditor.Retrofit.ImgurService;
-import com.guanqing.subredditor.Retrofit.Models.GalleryModel;
-import com.guanqing.subredditor.Retrofit.Models.ImageData;
-import com.guanqing.subredditor.Retrofit.Models.ImageModel;
-import com.guanqing.subredditor.UI.Fragments.ZoomDialog;
-import com.guanqing.subredditor.Util.ImgurUtil;
-import com.guanqing.subredditor.Util.ToastUtil;
-import com.squareup.okhttp.ResponseBody;
+import com.guanqing.subredditor.Util.ListenerUtil;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.Response;
-import retrofit.Retrofit;
 
 
 public class FrontPageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<FrontPageModel> dataSet;
 
-    private Context context;
+    private Context mContext;
 
-    private FragmentManager fm;
     private FrontPageFeedViewHolder holder;
 
     public FrontPageAdapter(Context context) {
-        this.context = context;
+        mContext = context;
         dataSet = new ArrayList<>();
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        final View view = LayoutInflater.from(context).inflate(R.layout.frontpage_list_item, parent, false);
-        fm = ((FragmentActivity) context).getSupportFragmentManager();
+        final View view = LayoutInflater.from(mContext).inflate(R.layout.frontpage_list_item, parent, false);
         return new FrontPageFeedViewHolder(view);
     }
 
@@ -68,37 +49,41 @@ public class FrontPageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         final FrontPageModel frontpageModel = dataSet.get(position);
         // configure a proper onClickListener for each case
         // imgur image, gif, gallery, not imgur, etc.
-        View.OnClickListener onClickListener = getProperOnClickListener(frontpageModel);
+        View.OnClickListener onClickListener = ListenerUtil.getProperOnClickListener(mContext, frontpageModel);
         boolean isGif = false;
         //detect whether the content is animated gif
         if (frontpageModel.link.endsWith(".gif") || frontpageModel.link.endsWith(".gifv")){
             isGif = true;
         }
-        //set gif icon visibility
-        holder.ivGifIcon.setVisibility(isGif ? View.VISIBLE : View.GONE);
 
         if (frontpageModel.thumbnailUrl == null) {
             //if there is no pic, do not inflate imageview & increase the maxLines of the title to 5
-            holder.ivThumbnail.setImageResource(android.R.color.transparent);
-            holder.ivThumbnail.setMaxHeight(0);
+            holder.ivThumbnail.setVisibility(View.GONE);
             holder.tvFeedTitle.setMaxLines(5);
         } else {
             //set image res and onClickListener
-            Glide.with(context).load(frontpageModel.thumbnailUrl)
-                    //.placeholder(R.drawable.avatar_loading)
+            holder.ivThumbnail.setVisibility(View.VISIBLE);
+            holder.tvFeedTitle.setMaxLines(3);
+            Glide.with(mContext).load(frontpageModel.thumbnailUrl)
+                    .placeholder(R.drawable.avatar_loading)
                     .error(R.drawable.error_gray)
                     .thumbnail(0.1f)
                     .crossFade()
                     .into(holder.ivThumbnail);
         }
-
+        //set thumbnail's on click listener
         holder.ivThumbnail.setOnClickListener(onClickListener);
+
+        //set gif icon visibility
+        holder.ivGifIcon.setVisibility(isGif ? View.VISIBLE : View.GONE);
 
         //set karma number
         holder.tvKarma.setText(frontpageModel.karma + "");
+
         //set title text and onClickListener
         holder.tvFeedTitle.setText(frontpageModel.title);
         holder.tvFeedTitle.setOnClickListener(onClickListener);
+
         //set comment number
         holder.tvCommentsNum.setText(frontpageModel.commentCount+"");
     }
@@ -158,178 +143,6 @@ public class FrontPageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         dataSet.clear();
         notifyDataSetChanged();
     }
-
-
-    /**
-     * =====================================================================================
-     * ============================== HELPER FUNCTIONS =====================================
-     * =====================================================================================
-     */
-    private View.OnClickListener getProperOnClickListener(final FrontPageModel frontpageModel){
-        //dismiss any dialog which is currently showing on top
-        Fragment prev = ((FragmentActivity)context).getSupportFragmentManager().findFragmentByTag(ZoomDialog.DIALOG_FLAG);
-        if (prev != null){
-            ZoomDialog df = (ZoomDialog) prev;
-            df.dismiss();
-            ((FragmentActivity)context).getSupportFragmentManager().beginTransaction().remove(prev);
-        }
-
-        View.OnClickListener onClickListener;
-        ImgurClient client = ImgurClient.getInstance();
-        client.configureRestAdapter();
-        ImgurService service;
-        final String link = frontpageModel.link;
-
-        //detect link type
-        ImgurUtil.LinkType linkType = ImgurUtil.getLinkType(link);
-        switch (linkType){
-            case NOT_IMGUR:
-                onClickListener = new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //TODO: new webview
-                        ToastUtil.show(link);
-                    }
-                };
-                break;
-
-            case IMGUR_GALLERY:
-                //get the links of the images in the gallery using Imgur API
-                service = client.getClient(ImgurService.class);
-                Call<GalleryModel> galleryCall = service.getGallery(ImgurUtil.getId(link));
-                galleryCall.enqueue(new Callback<GalleryModel>() {
-                    @Override
-                    public void onResponse(Response<GalleryModel> response, Retrofit retrofit) {
-                        GalleryModel gallery = response.body();
-
-                        if (gallery == null) {
-                            //404 or the response cannot be converted to ImageModel.
-                            ResponseBody responseBody = response.errorBody();
-                            if (responseBody != null) {
-                                Log.e("HGQ", "responseBody = " + responseBody.toString());
-                            } else {
-                                Log.e("HGQ", "responseBody = null");
-                            }
-                        } else {
-                            //200 - success
-                            //set the correct links in the model
-                            if(gallery.getData().isAlbum()){
-                                List<String> list = new ArrayList<>();
-                                for(ImageData imageData: gallery.getData().getImages()){
-                                    if (imageData.isAnimated()){
-                                        list.add(imageData.getMp4());
-                                    }else{
-                                        list.add(imageData.getLink());
-                                    }
-                                }
-                                frontpageModel.setLinks(list);
-                            }else{
-                                if (gallery.getData().isAnimated()){
-                                    frontpageModel.setLink(gallery.getData().getMp4());
-                                }else{
-                                    frontpageModel.setLink(gallery.getData().getLink());
-                                }
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Throwable t) {
-                        Log.e("HGQ", "t = " + t.getMessage());
-                    }
-                });
-                //set on click listener
-                onClickListener = new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //TODO: new gallery view
-                        ZoomDialog fragment = ZoomDialog.newInstance(frontpageModel);
-                        fragment.show(fm, ZoomDialog.DIALOG_FLAG);
-                    }
-                };
-                break;
-
-            case IMGUR_LINK:
-                //get the link of the image using Imgur API
-                service = client.getClient(ImgurService.class);
-                Call<ImageModel> imageCall = service.getImage(ImgurUtil.getId(link));
-                imageCall.enqueue(new Callback<ImageModel>() {
-                    @Override
-                    public void onResponse(Response<ImageModel> response, Retrofit retrofit) {
-                        ImageModel image = response.body();
-                        if (image == null) {
-                            //404 or the response cannot be converted to ImageModel.
-                            ResponseBody responseBody = response.errorBody();
-                            if (responseBody != null) {
-                                Log.e("HGQ","responseBody = " + responseBody.toString());
-                            } else {
-                                Log.e("HGQ","responseBody = null");
-                            }
-                        } else {
-                            //200 - success
-                            //set the correct link in the model
-                            if (image.getData().isAnimated()){
-                                frontpageModel.setLink(image.getData().getMp4());
-                            }else{
-                                frontpageModel.setLink(image.getData().getLink());
-                            }
-                        }
-                    }
-                    @Override
-                    public void onFailure(Throwable t) {
-                        Log.e("HGQ", "t = " + t.getMessage());
-                    }
-                });
-
-                //set on click listener
-                onClickListener = new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        ZoomDialog fragment = ZoomDialog.newInstance(frontpageModel);
-                        fragment.show(fm, ZoomDialog.DIALOG_FLAG);
-                    }
-                };
-                break;
-
-            case GIF:
-                if(link.contains("imgur")) {
-                    //change the gif link to mp4 if the GIF is from imgur.com
-                    if (link.endsWith(".gifv")) {
-                        frontpageModel.setLink(link.substring(0, link.length()-4) + "gif");
-                    }
-                    if (link.endsWith(".gif")) {
-                        frontpageModel.setLink(link.substring(0, link.length()-3) + "gif");
-                    }
-                }
-                onClickListener = new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //show new detailed dialog
-                        ZoomDialog fragment = ZoomDialog.newInstance(frontpageModel);
-                        fragment.show(fm, ZoomDialog.DIALOG_FLAG);
-                    }
-                };
-                break;
-
-            case IMAGE:
-                onClickListener = new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //show new detailed dialog
-                        ZoomDialog fragment = ZoomDialog.newInstance(frontpageModel);
-                        fragment.show(fm, ZoomDialog.DIALOG_FLAG);
-                    }
-                };
-                break;
-
-            default:
-                onClickListener = null;
-                break;
-
-        }
-        return onClickListener;
-    }
-
 
 
 
--- app/src/main/java/com/guanqing/subredditor/Util/ImgurUtil.java
+++ app/src/main/java/com/guanqing/subredditor/Util/ImgurUtil.java
@@ -1,14 +1,5 @@
 package com.guanqing.subredditor.Util;
 
-import android.support.annotation.Nullable;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-
 /**
  * Created by Guanqing on 2015/12/29.
  */
@@ -33,83 +24,5 @@ public class ImgurUtil {
 
     public static String getId(String link){
         return link.substring(link.lastIndexOf("/"), link.length());
-    }
-
-    /**
-     * get image link via Imgur API
-     * @param webpageUrl
-     * @return imageUrl
-     */
-    public static String getLink(String webpageUrl) throws Exception{
-        webpageUrl = convertImgurUrl(webpageUrl);
-        String json = readUrl(webpageUrl);
-        JSONObject data = new JSONObject(json).getJSONObject("data");
-        if(data.getString("type").equals("image/gif")){
-            return data.getString("mp4");
-        }
-        return data.getString("link");
-    }
-
-
-    /**
-     * get the image link in another way
-     * when caught FileNotFoundException connecting to the Imgur API
-     * @param url
-     * @return suedoImageUrl
-     */
-    @Nullable
-    public static String getLinkWhenError(String url){
-        if(url==null) return null;
-        //download the file as MP4 first since we don't know what format it is
-        //if Glide failed to decode the file, rename the file to JPG
-        return url.replace("imgur.com", "i.imgur.com") + ".mp4";
-    }
-
-    /**
-     * get the links from an imgur gallery via Imgur API
-     * @param webpageUrl
-     * @return imageLinks
-     * @throws Exception
-     */
-    public static String[] getLinksFromGallery(String webpageUrl) throws Exception{
-        webpageUrl = convertGalleryUrl(webpageUrl);
-        String json = readUrl(webpageUrl);
-        JSONObject data = new JSONObject(json).getJSONObject("data");
-        int imagesCount = data.getInt("images_count");
-        JSONArray images = data.getJSONArray("images");
-
-        String[] links = new String[imagesCount];
-        for (int i=0;i<imagesCount;i++){
-            JSONObject o = images.getJSONObject(i);
-            links[i] = o.getString("link");
-        }
-        return links;
-    }
-
-    private static String convertImgurUrl(String url){
-        return url.replace("http://imgur.com/", "https://api.imgur.com/3/image/");
-    }
-
-    private static String convertGalleryUrl(String url){
-        return url.replace("http://imgur.com/", "https://api.imgur.com/3/gallery/");
-    }
-
-    //read json string from url
-    private static String readUrl(String urlString) throws Exception {
-        BufferedReader reader = null;
-        try {
-            URL url = new URL(urlString);
-            reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            StringBuffer buffer = new StringBuffer();
-            int read;
-            char[] chars = new char[1024];
-            while ((read = reader.read(chars)) != -1)
-                buffer.append(chars, 0, read);
-
-            return buffer.toString();
-        } finally {
-            if (reader != null)
-                reader.close();
-        }
     }
 }
